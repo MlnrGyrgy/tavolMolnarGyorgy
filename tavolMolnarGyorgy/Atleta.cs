6ede83ba6fb39749ed8e8fc920dd19d8e0523fca
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-10-13 08:31:52 +0200

    1. feladat: Beolvasas

--- tavolMolnarGyorgy/Atleta.cs
+++ tavolMolnarGyorgy/Atleta.cs
@@ -6,10 +6,21 @@
 
         public string VezNev { get; private set; }
 
-        public string KerNev{ get; private set; }
+        public string KerNev { get; private set; }
 
-        public string Egyesulet{ get; private set; }
+        public string Egyesulet { get; private set; }
 
         public int Ugras { get; private set; }
+     
+        public Atleta(string sor)
+        {
+            string[] egy = sor.Split(';');
+            Rajtszam = egy[0];
+            VezNev = egy[1];
+            KerNev = egy[1];
+            Egyesulet = egy[2];
+            Ugras = int.Parse(egy[3]);
+        }
+       
     }
 }
--- tavolMolnarGyorgy/Program.cs
+++ tavolMolnarGyorgy/Program.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace tavolMolnarGyorgy
 {
     class Program
     {
+        static List<Atleta> adatok = new List<Atleta>();
+        static void Beolvas()
+        {
+            StreamReader file = new StreamReader("tavol.csv");
+            while (!file.EndOfStream)
+            {
+                adatok.Add(new Atleta(file.ReadLine()));
+            }
+            file.Close();
+        }
         static void Main(string[] args)
         {
-           
+            Beolvas();
+            foreach (var i in adatok)
+            {
+               Console.WriteLine(i.Rajtszam+ " " + i.VezNev+" "+ i.KerNev+" "+ i.Ugras);
+            }
+            Console.ReadKey();
         }
     }
 }

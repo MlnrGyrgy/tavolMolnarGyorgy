e8ba36f0de8230d937a3bf3e299c5b05eff02be5
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-10-13 08:07:14 +0200

    Add project files.

---
+++ tavolMolnarGyorgy/Atleta.cs
@@ -0,0 +1,15 @@
+﻿namespace tavolMolnarGyorgy
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+
+        public string VezNev { get; private set; }
+
+        public string KerNev{ get; private set; }
+
+        public string Egyesulet{ get; private set; }
+
+        public int Ugras { get; private set; }
+    }
+}
